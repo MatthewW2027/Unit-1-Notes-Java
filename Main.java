0759b95ab78d299aaf6355133e9cfc0fa55be2f9
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-09-12 12:56:07 +0000

    1.2 notes

--- Main.java
+++ Main.java
@@ -4,5 +4,20 @@ public class Main {
    public static void main(String []args) {
       System.out.println("Welcome to Java");
       System.out.println("hiiiii");
+
+      // 1. DECLARE a new variable 
+      int score; 
+      // 2. ASSIGN a value to the variable 
+      score = 64;
+      // INITIALIZE combines declaring + assigning 
+      String pokeman = "Squirtle";
+      double healthPercent = 0.75; 
+      boolean isAlive = true; 
+      
+      // Use the plus sign to CONCATENATE (combine) Strings 
+      String greeting = "Hello " + "World"; 
+      System.out.println(greeting);
+      // Quatation marks contain STRING LITERALS  
+
    }
 }
