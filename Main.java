c516995b86bc50a9c1187c00dea3547fec9384d2
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-09-16 17:40:55 +0000

    math

--- Main.java
+++ Main.java
@@ -17,7 +17,37 @@ public class Main {
       // Use the plus sign to CONCATENATE (combine) Strings 
       String greeting = "Hello " + "World"; 
       System.out.println(greeting);
-      // Quatation marks contain STRING LITERALS  
+      // Quatation marks contain STRING LITERALS 
+      
+      //OUTPUT: difference between.print and .println
+      System.out.print("hi"); 
+      System.out.println("there"); 
+      System.out.print("!");
+      //.println dispalys the meesage THEN goes to the next line 
+
+      //ESCAPE: use \ (backslash) to include special characters, use \n for line break 
+      String message = "\nI woke up and said, \"HELLOW WORLD\""; 
+      System.out.println(message);
+
+      // ARITHMETIC expressions with operators 
+      System.out.println(4+3); 
+      System.out.println(4-1); 
+      System.out.println(6*7); 
+      System.out.println(5/3); //TRUNCATED! 
+      // Integer division results in TRUNCATION, which is basically throwing away the decimal place
+      System.out.println(5.0/3); //double 
+
+      //COMPOUND expressions follow PEMDAS 
+      double result = 2+(3*2-6); 
+      System.out.println(result); 
+
+      //REMAINDER % operator 
+      int remain = 23 % 2; //1-> means ODD number 
+      System.out.println(remain); 
+      System.out.println(629 % 2); //0-> means EVEN 
+
+      //Watch out for the plus sign! 
+      System.out.println(3+"3");
 
    }
 }
